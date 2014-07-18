70927efae4a66d971bd014dbe8a8fbdd57afdc6f
Shantanu H. Joshi <s.joshi@ucla.edu>
2014-07-17 23:05:55 -0700

    changed formatting for modelspec. added functionality for measures such as correlation

--- bss/modelspec.py
+++ bss/modelspec.py
@@ -11,26 +11,31 @@ __credits__ = 'Contributions and ideas: Shantanu H. Joshi, Roger P. Woods, David
 
 import ConfigParser
 import re
-
+import sys
 
 class ModelSpec(object):
 
     def __init__(self, modelfile):
-        self.subjdir = ''
         self.subjectid = ''
         self.demographics = ''
-        self.phenotype = ''
-        self.phenotype_attribute_matrix_file = ''
+        self.fileid = ''
+        self.atlas_surface = ''
 
         self.modeltype = ''
         self.fullmodel = ''
         self.nullmodel = ''
         self.stat_test = ''
+
         self.variables = ''
         self.unique = ''
         self.factors = []
-        self.atlas_surface = ''
+
+        self.read_success = True
         self.read_modelfile(modelfile)
+
+        if self.read_success == False:
+            return
+
         self.parse_model()
 
     def read_modelfile(self, modelfile):
@@ -38,32 +43,65 @@ class ModelSpec(object):
         config.read(modelfile)
 
         config.options(config.sections()[0])
-        self.subjdir = config.get('subjectinfo', 'subjectdir')
         self.subjectid = config.get('subjectinfo', 'subjectid')
         self.demographics = config.get('subjectinfo', 'demographics')
-        self.phenotype = config.get('subjectinfo', 'phenotype')
-        self.phenotype_attribute_matrix_file = config.get('subjectinfo', 'phenotype_attribute_matrix')
-        self.atlas_surface = config.get('subjectinfo', 'atlas_surface')
+        self.fileid = config.get('subjectinfo', 'fileid')
+        self.atlas_surface = config.get('subjectinfo', 'atlas')
 
-        self.modeltype = config.get('model', 'modeltype')
-        self.fullmodel = config.get('model', 'fullmodel')
-        self.nullmodel = config.get('model', 'nullmodel')
-        factorstring = config.get('model', 'factors')
-        for i in re.split(' ', factorstring):
-            self.factors.append(i.rstrip().lstrip())
+        self.read_success = False
+        self.model_flag = False
+        self.measure_flag = False
+        try:
+            self.modeltype = config.get('model', 'modeltype')
+            self.fullmodel = config.get('model', 'fullmodel')
+            self.nullmodel = config.get('model', 'nullmodel')
+            self.stat_test = config.get('model', 'test')
+            self.model_flag = True
+            self.read_success = True
+        except ConfigParser.NoSectionError:
+            self.read_success = False
 
-        self.stat_test = config.get('model', 'test')
+        try:
+            self.coeff = config.get('measure', 'coeff')
+            self.variable = config.get('measure', 'variable')
+            self.stat_test = self.coeff
+            self.measure_flag = True
+            self.read_success = True
+        except ConfigParser.NoSectionError:
+            self.read_success = False
+
+        if self.model_flag and self.measure_flag:
+            sys.stdout.write('The modelspec.ini can only contain one statistical design ([model] or [measure]).'
+                             'Multiple designs are not currently supported, but this may change in the future.\n')
+            self.read_success = False
+            return
+
+        if self.model_flag:
+            self.read_success = True
+            return
+
+        if self.measure_flag:
+            self.read_success = True
+            return
+
+
+        # factorstring = config.get('model', 'factors')
+        # for i in re.split(' ', factorstring):
+        #     self.factors.append(i.rstrip().lstrip())
 
     def parse_model(self):
-        # Parse fullmodel and nullmodel
-        set_full = set()
-        set_null = set()
-        for i in re.split('\+|', self.fullmodel):
-            set_full.add(i.rstrip().lstrip())
-        for i in re.split('\+', self.nullmodel):
-            set_null.add(i.rstrip().lstrip())
-        self.variables = set_full | set_null
-        self.unique = list(set_full - set_null)[0]  # TODO check: only one element should be present
+        if self.model_flag:
+            # Parse fullmodel and nullmodel
+            set_full = set()
+            set_null = set()
+            for i in re.split('\+|', self.fullmodel):
+                set_full.add(i.rstrip().lstrip())
+            for i in re.split('\+', self.nullmodel):
+                set_null.add(i.rstrip().lstrip())
+            self.unique = list(set_full - set_null)[0]  # TODO check: only one element should be present
+        elif self.measure_flag:
+            # self.variables = set_full | set_null
+            return
 
     def validate_model(self):
         #TODO validate model
