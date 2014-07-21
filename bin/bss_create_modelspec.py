fbe1024a05e85f02c0ceb8b50cfe87924afc9a19
Shantanu H. Joshi <s.joshi@ucla.edu>
2014-07-21 09:49:39 -0700

    Exception handling. Currently generic.

--- bin/bss_create_modelspec.py
+++ bin/bss_create_modelspec.py
@@ -13,7 +13,7 @@ __credits__ = 'Contributions and ideas: Shantanu H. Joshi, Roger P. Woods, David
 
 
 import argparse
-import sys
+import sys, traceback
 from bss.modelspec import ModelSpec
 import os
 import pandas
@@ -29,103 +29,113 @@ def main():
 
 def bss_create_modelspec(modelspec_file):
 
-    sys.stdout.write('This program will help you generate a modelspec.ini file\n')
-    sys.stdout.write('It is recommended to have the demographics spreadsheet with the subjects and the covariates ready.\n')
-    sys.stdout.write('Additionally, the spreadsheet should have a column called File that specifies the dfs files you want to analyze.\n')
-    sys.stdout.write('I will ask you a series of questions to help create the modelspec.ini.\n')
-    sys.stdout.write("Let's start with the subject information first....\n")
-    sys.stdout.write("===================================================================\n")
-    sys.stdout.write("========================Subject Information========================\n")
-    sys.stdout.write("===================================================================\n")
-
-    while True:
-        atlas = raw_input('Specify the location of the BrainSuite atlas (Type in full path of the *.pial.cortex.dfs file in the atlas directory):').rstrip()
-        if os.path.exists(atlas):
-            break
-        else:
-            sys.stdout.write('Error: Atlas ' + atlas + ' does not exist. Please re-enter.\n')
-
-    while True:
-        demographics_csv = raw_input('Location of the demographics file (This will be a csv file):').rstrip()
-        if os.path.exists(demographics_csv):
-            break
-        else:
-            sys.stdout.write('Error: Demographics file ' + demographics_csv + ' does not exist. Please re-enter.\n')
-
-
-    sys.stdout.write("Parsing the demographics file....")
-    demographic_data = StatsData.read_demographics(demographics_csv)
-
-    while True:
-        subjid = raw_input('Specify the unique subject identifier column name in the demographics file (subject ID for e.g.):')
-        if subjid in demographic_data.columns:
-            break
-        else:
-            sys.stdout.write('The subject identifier column name does not exist in ' + demographics_csv + '. Please re-enter.\n')
-    while True:
-        file_id = raw_input('Specify the File identifier column name in the demographics file (column that contains file names to analyze):').rstrip()
-        if file_id in demographic_data.columns:
-            break
-        else:
-            sys.stdout.write('The File identifier column name does not exist in ' + demographics_csv + '. Please re-enter.\n')
-
-    sys.stdout.write('Done.\n')
-    sys.stdout.write("Let's specify the statistical model information....\n")
-    sys.stdout.write("=================================================================\n")
-    sys.stdout.write("========================Model Information========================\n")
-    sys.stdout.write("=================================================================\n")
-
-    while True:
-        modeltype = raw_input('Specify the type of model or measure (lm/corr):').rstrip().lower()
-        if modeltype == 'lm' or 'corr':
-            break
-        else:
-            sys.stdout.write('Incorrect choice. Please re-enter.\n')
-
-    if modeltype == 'lm':
-
+    try:
+        sys.stdout.write('This program will help you generate a modelspec.ini file\n')
+        sys.stdout.write('It is recommended to have the demographics spreadsheet with the subjects and the covariates ready.\n')
+        sys.stdout.write('Additionally, the spreadsheet should have a column called File that specifies the dfs files you want to analyze.\n')
+        sys.stdout.write('I will ask you a series of questions to help create the modelspec.ini.\n')
+        sys.stdout.write("Let's start with the subject information first....\n")
+        sys.stdout.write("===================================================================\n")
+        sys.stdout.write("========================Subject Information========================\n")
+        sys.stdout.write("===================================================================\n")
 
         while True:
-            main_effect = raw_input('Specify the variable name for the main effect you want to test:').rstrip()
-            if main_effect in demographic_data.columns:
+            atlas = raw_input('Specify the location of the BrainSuite atlas (Type in full path of the *.pial.cortex.dfs file in the atlas directory):').rstrip()
+            if os.path.exists(atlas):
                 break
             else:
-                sys.stdout.write('The variable name for the main effect does not exist in ' + demographics_csv + '. Please re-enter.\n')
+                sys.stdout.write('Error: Atlas ' + atlas + ' does not exist. Please re-enter.\n')
 
         while True:
-            covariates_string = raw_input('Specify the covariates (comma separated) you want to control for:').rstrip()
-            covariates_list = covariates_string.split(',')
-            covariates_list = [i.rstrip().lstrip() for i in covariates_list]
-            if set(covariates_list).issubset(set(demographic_data.columns)):
+            demographics_csv = raw_input('Location of the demographics file (This will be a csv file):').rstrip()
+            if os.path.exists(demographics_csv):
                 break
             else:
-             sys.stdout.write('The covariate variable name(s) does not exist in ' + demographics_csv + '. Please re-enter.\n')
-
-        fullmodel = main_effect + ' + ' + ' + '.join(covariates_list)
-        nullmodel = ' + '.join(covariates_list)
-        test = 'anova'
-
-    elif modeltype == 'corr':
-        pass
+                sys.stdout.write('Error: Demographics file ' + demographics_csv + ' does not exist. Please re-enter.\n')
 
 
+        sys.stdout.write("Parsing the demographics file....")
+        demographic_data = StatsData.read_demographics(demographics_csv)
 
-    sys.stdout.write('Saving the modelspec file + ' + modelspec_file + '...')
-    fid = open(modelspec_file, 'wt')
-    fid.write('[subjectinfo]\n')
-    fid.write('subjectid={0:s}\n'.format(subjid))
-    fid.write('demographics={0:s}\n'.format(demographics_csv))
-    fid.write('fileid={0:s}\n'.format(file_id))
-    fid.write('atlas={0:s}\n'.format(atlas))
+        while True:
+            subjid = raw_input('Specify the unique subject identifier column name in the demographics file (subject ID for e.g.):')
+            if subjid in demographic_data.columns:
+                break
+            else:
+                sys.stdout.write('The subject identifier column name does not exist in ' + demographics_csv + '. Please re-enter.\n')
+        while True:
+            file_id = raw_input('Specify the File identifier column name in the demographics file (column that contains file names to analyze):').rstrip()
+            if file_id in demographic_data.columns:
+                break
+            else:
+                sys.stdout.write('The File identifier column name does not exist in ' + demographics_csv + '. Please re-enter.\n')
 
-    fid.write('\n[model]\n')
-    fid.write('modeltype={0:s}\n'.format(modeltype))
-    fid.write('fullmodel={0:s}\n'.format(fullmodel))
-    fid.write('nullmodel={0:s}\n'.format(nullmodel))
-    fid.write('test={0:s}\n'.format('anova'))
-    fid.close()
-    sys.stdout.write('Done.\n')
+        sys.stdout.write('Done.\n')
+        sys.stdout.write("Let's specify the statistical model information....\n")
+        sys.stdout.write("=================================================================\n")
+        sys.stdout.write("========================Model Information========================\n")
+        sys.stdout.write("=================================================================\n")
 
+        while True:
+            modeltype = raw_input('Specify the type of model or measure (lm/corr):').rstrip().lower()
+            if modeltype == 'lm' or 'corr':
+                break
+            else:
+                sys.stdout.write('Incorrect choice. Please re-enter.\n')
+
+        if modeltype == 'lm':
+
+
+            while True:
+                main_effect = raw_input('Specify the variable name for the main effect you want to test:').rstrip()
+                if main_effect in demographic_data.columns:
+                    break
+                else:
+                    sys.stdout.write('The variable name for the main effect does not exist in ' + demographics_csv + '. Please re-enter.\n')
+
+            while True:
+                covariates_string = raw_input('Specify the covariates (comma separated) you want to control for:').rstrip()
+                covariates_list = covariates_string.split(',')
+                covariates_list = [i.rstrip().lstrip() for i in covariates_list]
+                if set(covariates_list).issubset(set(demographic_data.columns)):
+                    break
+                else:
+                 sys.stdout.write('The covariate variable name(s) does not exist in ' + demographics_csv + '. Please re-enter.\n')
+
+            fullmodel = main_effect + ' + ' + ' + '.join(covariates_list)
+            nullmodel = ' + '.join(covariates_list)
+            test = 'anova'
+
+        elif modeltype == 'corr':
+            pass
+
+
+
+        sys.stdout.write('Saving the modelspec file + ' + modelspec_file + '...')
+        fid = open(modelspec_file, 'wt')
+        fid.write('[subjectinfo]\n')
+        fid.write('subjectid={0:s}\n'.format(subjid))
+        fid.write('demographics={0:s}\n'.format(demographics_csv))
+        fid.write('fileid={0:s}\n'.format(file_id))
+        fid.write('atlas={0:s}\n'.format(atlas))
+
+        fid.write('\n[model]\n')
+        fid.write('modeltype={0:s}\n'.format(modeltype))
+        fid.write('fullmodel={0:s}\n'.format(fullmodel))
+        fid.write('nullmodel={0:s}\n'.format(nullmodel))
+        fid.write('test={0:s}\n'.format('anova'))
+        fid.close()
+        sys.stdout.write('Done.\n')
+
+    except IOError as (errno, strerror):
+        print "I/O error({0}): {1}".format(errno, strerror)
+    except:
+        print "Something went wrong. Please send this error message to the developers." \
+              "\nUnexpected error:", sys.exc_info()[0]
+        print traceback.print_exc(file=sys.stdout)
+
+
+    sys.exit(0)
 
 
 if __name__ == '__main__':
--- bin/bss_run.py
+++ bin/bss_run.py
@@ -19,7 +19,7 @@ from bss.modelspec import ModelSpec
 from bss.stats_data import StatsData
 from bss.stats_engine import StatsEngine
 import os
-import sys
+import sys, traceback
 
 
 def main():
@@ -39,36 +39,42 @@ def main():
 
 def bss_run(modelspec, outdir, opt_statsengine):
 
-    outprefix = ''
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-    logging.basicConfig(filename=os.path.join(outdir, 'bss.log'), level=logging.DEBUG,
-                        format='%(levelname)s:%(message)s', filemode='w')
-    logging.info('Reading model file ' + modelspec)
-    model = ModelSpec(modelspec)
-    logging.info('Done.')
-    if model.read_success == False:
-        sys.stdout.write('The modelspec file is incorrectly formatted. Perhaps it is missing some sections/fields.\n')
-        sys.exit()
-
-    logging.info('Reading demographics file ' + model.demographics + ' and creating a data frame...')
-    statsdata = StatsData(model.demographics, model)
-    logging.info('Done.')
-
-    if statsdata.data_read_flag:
-        logging.info('Computing ' + model.modeltype + ' with ' + model.stat_test + '...')
-        statsengine = StatsEngine(model, statsdata, engine=opt_statsengine)
-        statsoutput = statsengine.run()
-        if model.measure_flag:
-            outprefix = model.stat_test + '_' + model.variable + '_' + os.path.splitext(os.path.split(model.atlas_surface)[1])[0]
-        elif model.model_flag:
-            outprefix = model.stat_test + '_' + model.unique  + '_' + os.path.splitext(os.path.split(model.atlas_surface)[1])[0]
-        statsoutput.save(outdir, outprefix, model.atlas_surface)
+    try:
+        outprefix = ''
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        logging.basicConfig(filename=os.path.join(outdir, 'bss.log'), level=logging.DEBUG,
+                            format='%(levelname)s:%(message)s', filemode='w')
+        logging.info('Reading model file ' + modelspec)
+        model = ModelSpec(modelspec)
         logging.info('Done.')
-    else:
-        sys.stdout.write('Problem in reading all the cortical attributes. '
-                         'Please check if the individual cortical surfaces are registered to the same atlas.\n')
-        sys.stdout.write('Exiting the statistical analysis.\n')
+        if model.read_success == False:
+            sys.stdout.write('The modelspec file is incorrectly formatted. Perhaps it is missing some sections/fields.\n')
+            sys.exit()
+
+        logging.info('Reading demographics file ' + model.demographics + ' and creating a data frame...')
+        statsdata = StatsData(model.demographics, model)
+        logging.info('Done.')
+
+        if statsdata.data_read_flag:
+            logging.info('Computing ' + model.modeltype + ' with ' + model.stat_test + '...')
+            statsengine = StatsEngine(model, statsdata, engine=opt_statsengine)
+            statsoutput = statsengine.run()
+            if model.measure_flag:
+                outprefix = model.stat_test + '_' + model.variable + '_' + os.path.splitext(os.path.split(model.atlas_surface)[1])[0]
+            elif model.model_flag:
+                outprefix = model.stat_test + '_' + model.unique  + '_' + os.path.splitext(os.path.split(model.atlas_surface)[1])[0]
+            statsoutput.save(outdir, outprefix, model.atlas_surface)
+            logging.info('Done.')
+        else:
+            sys.stdout.write('Problem in reading all the cortical attributes. '
+                             'Please check if the individual cortical surfaces are registered to the same atlas.\n')
+            sys.stdout.write('Exiting the statistical analysis.\n')
+    except:
+        print "Something went wrong. Please send this error message to the developers." \
+              "\nUnexpected error:", sys.exc_info()[0]
+        print traceback.print_exc(file=sys.stdout)
+
 
 if __name__ == '__main__':
     main()
